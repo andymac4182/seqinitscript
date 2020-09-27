fafe85e75730038022110ce3ef3e1abf3a527185
Andrew McClenaghan <andymac4182@users.noreply.github.com>
2020-09-28 07:56:39 +1000

    Update PATH

--- init-runner.sh
+++ init-runner.sh
@@ -9,12 +9,14 @@ ERRCODE=1
 start_time="$(date -u +%s)"
 end_time="$(date -u +%s)"
 
-/bin/seqcli/seqcli config -k connection.serverUrl -v http://localhost/
+PATH=~/bin/seqcli:$PATH
+
+seqcli config -k connection.serverUrl -v http://localhost/
 
 echo "Monitoring for seq server coming online"
 
 while [[ $ERRCODE -ne 0 ]]; do # || [[ $((end_time-start_time)) -lt 120 ]]
-	/bin/seqcli/seqcli signal list > /dev/null 2>&1
+	seqcli signal list > /dev/null 2>&1
 	ERRCODE=$?
 	sleep 1
 	end_time="$(date -u +%s)"
